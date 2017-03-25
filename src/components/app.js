823bdad35a81923a4ce23748f5150f0ca5bc6d5f
William Dunlop <william.dunlop.1993@gmail.com>
2017-03-25 15:57:56 +1100

    created the 'Term' component

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import Cube from './cube/cube';
 import SmallCube from './cube/small_cube';
+import Term from './scrolling_text';
 
 export default class App extends Component {
   render() {
     return (
       <div className="row top-buffer">
         <div className="col-md-6 col-md-offset-3">
-          <div className="panel panel-default">
+          <div className="panel panel-primary">
             <div className="panel-heading">
               <h3 className="panel-title">Will's Wonderful Cube</h3>
             </div>
-            <div className="panel-body">
+            <div className="panel-body black">
               <div className="big-space">
                 <Cube />
                 <SmallCube />
+                <div className="brk" />
+                <Term />
               </div>
             </div>
           </div>
---
+++ src/components/scrolling_text.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Term = () =>
+  <div className="scroll-up">
+    <div>
+      <con><b>>>>[con@sole proj]$</b> npm start</con>
+      <br />
+      <con>>>>> base-redux-project@1.0.0 start /home/con/projects/proj</con>
+      <con>>>>> node ./node_modules/webpack-dev-server/bin/webpack-dev-server.js</con>
+      <br />
+      <con>>>> http://localhost:8080/webpack-dev-server/</con>
+      <con>>>>webpack result is served from /</con>
+      <con>>>>content is served from ./</con>
+      <con>>>>404s will fallback to /index.html</con>
+      <con>>>><b>Hash:</b> c17a3df2aa2821e43472</con>
+      <con>>>><b>Version:</b> webpack 1.14.0</con>
+      <br />
+      <con>>>><b>Time:</b> 173ms</con>
+      <con>>>><b>    Asset    Size  Chunks             Chunk Names</b></con>
+      <con>>>>bundle.js  754 kB       0  [emitted]  main</con>
+      <con>>>>chunk    (0) bundle.js (main) 714 kB [rendered]</con>
+      <con>>>>  [203] ./src/components/scrolling_text.jsx 2.43 kB (0) [built]</con>
+      <con>>>>     + 204 hidden modules</con>
+      <con>>>><b>webpack:</b> Compiled successfully.</con>
+      <con>>>><b>[con@sole proj]$</b> npm start</con>
+      <br />
+      <con>>>>> base-redux-project@1.0.0 start /home/con/projects/proj</con>
+      <con>>>>> node ./node_modules/webpack-dev-server/bin/webpack-dev-server.js</con>
+      <br />
+      <con>>>> http://localhost:8080/webpack-dev-server/</con>
+      <con>>>>webpack result is served from /</con>
+      <con>>>>content is served from ./</con>
+      <con>>>>404s will fallback to /index.html</con>
+    </div>
+  </div>;
+
+  export default Term;
